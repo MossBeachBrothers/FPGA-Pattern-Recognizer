9deaa6172f23b9aae5fd4fbb32defe6f79091069
Akhilendra Nair <mossbeachbrothers@gmail.com>
2024-08-20 17:11:39 -0700

    line memory with write enable, address, SRAM

---
+++ RTL/linemem.v
@@ -0,0 +1,26 @@
+module linemem (
+    input wire clk,
+    input wire reset,
+    input wire write_enable, 
+    input wire [7:0] data_in, //8 bit input pixel
+    output reg [7:0] data_out //8 bit output pixel
+);
+
+//Array for SRAM, 1280 elements, 8 bits wide
+reg [7:0] sram [0:1279]; 
+
+//read and write addresses. These are length 11 by convention (log2(1280) = 11)
+reg [10:0] write_address, read_address;
+
+always @(posedge clk or posedge reset) begin 
+    if (reset) begin 
+        //reset everything if reset
+    end else if (write_enable) begin 
+        //if write, write to memory
+    end 
+
+    //update addresses
+end
+
+endmodule 
+
